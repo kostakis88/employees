ccf345ee2550f8c723cd7f97fd1d6c2c00bf9716
Kostas Ntallas <kostasdallas2010@hotmail.com>
2020-12-14 23:25:26 +0200

    first commit

---
+++ employess-backend/src/test/java/com/example/employeesbackend/EmployeesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
